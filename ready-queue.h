8efa7fa898940639999fe4a4a5ec346f9a524f10
SteliosSpanos <stylspan19@gmail.com>
2025-11-26 00:33:44 +0200

    Removes unecessary function

--- ready-queue.h
+++ ready-queue.h
@@ -13,11 +13,11 @@ typedef struct queue {
 
 void init_queue(queue *q);
 
-int is_empty(queue *q);
+void free_queue(queue *q);
 
 int enqueue(queue *q, process p);
 
-int dequeue(queue *q);
+int dequeue(queue *q, process *p);
 
 void display_queue(queue *q);
 
