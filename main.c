2927305633243641ca1a51459aac4591566a729b
SteliosSpanos <stylspan19@gmail.com>
2025-11-29 20:35:36 +0200

    Updates main with latest process struct

--- main.c
+++ main.c
@@ -7,26 +7,21 @@
 
 int main() {
 	queue q;
-	init_queue(&q);
+  	init_queue(&q);
 
-	process p1, p2, p3;
-	init_process(&p1, 12);
-	init_process(&p2, 18);
-	init_process(&p3, 34);
+  	process p1, p2, p3;
+  	init_process(&p1, 12, 0, 8, 2);   // PID=12, arrival=0, burst=8, priority=2
+  	init_process(&p2, 18, 1, 4, 1);   // PID=18, arrival=1, burst=4, priority=1
+  	init_process(&p3, 34, 2, 9, 3);   // PID=34, arrival=2, burst=9, priority=3
 
-	enqueue(&q, p1);
-	enqueue(&q, p2);
-	enqueue(&q, p3);
+  	enqueue(&q, p1);
+  	enqueue(&q, p2);
+  	enqueue(&q, p3);
 
-	display_queue(&q);
+  	display_queue(&q);
 
-	process out;
-	dequeue(&q, &out);
-
-	display_queue(&q);
-	printf("Dequeue value: P%d\n", out.pid);
-
-	free_queue(&q);
-	exit(0);
+  	free_queue(&q);
+  	return 0;
+	
 }
 	
